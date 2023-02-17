f72b70020e3360364248d34cd1e2cb73826ed211
agron911 <agron0911@gmail.com>
2023-02-17 16:40:57 +0800

    update otc

--- fintect proj/crawler/trycrawl-otc.py
+++ fintect proj/crawler/trycrawl-otc.py
@@ -12,7 +12,7 @@ df = df.rename(columns={"Volume('000 shares)":"Volume"})
 df.sort_values(by='Date',inplace=True)
 
 df.set_index('Date',inplace=True)
-df.to_csv('./'+code+''.txt',sep='\t')
+df.to_csv('./'+code+'.txt',sep='\t')
 
 print(df)
 
