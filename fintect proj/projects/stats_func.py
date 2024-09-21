45447774b70ee4daf4aba0e405b58282b61edc1e
ChenghutS2024 <chenghut@andrew.cmu.edu>
2024-09-21 10:50:17 -0700

    fix twii empty/ stats

--- fintect proj/projects/stats_func.py
+++ fintect proj/projects/stats_func.py
@@ -1,8 +1,11 @@
 
 
 def long_tail(df):
-    three_years_peak = max(df.iloc[:1000,:].High)
-    two_years_low = min(df.iloc[600:, :].Low)
+    try:
+        three_years_peak = max(df.iloc[:1000,:].High)
+        two_years_low = min(df.iloc[600:, :].Low)
+    except:
+        return False
     if three_years_peak/two_years_low > 15:
         return True
     else:
